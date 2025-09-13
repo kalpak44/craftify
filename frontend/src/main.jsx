b218fdf97a172f01771b68e98b3b6cb5d6e9302a
Pavel Ushanli <pau@foryouandyourcustomers.com>
2025-09-13 11:56:10 +0300

    GitHub Pages workaround

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,14 @@ import {Auth0ProviderWithNavigate} from "./Auth0ProviderWithNavigate.jsx";
 
 const rootPath = import.meta.env.VITE_APP_ROOT_PATH;
 
+// GitHub Pages workaround
+const redirect = sessionStorage.redirect;
+if (redirect) {
+    sessionStorage.removeItem("redirect");
+    window.history.replaceState(null, null, redirect);
+}
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter basename={rootPath}>
