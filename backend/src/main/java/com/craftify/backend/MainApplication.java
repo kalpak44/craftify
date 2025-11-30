20ed07c54665cd99c972318685b19216655b04e1
Pavel Ushanli <pau@foryouandyourcustomers.com>
2025-11-30 14:42:29 +0200

    removed mongo dependencies

--- backend/src/main/java/com/craftify/backend/MainApplication.java
+++ backend/src/main/java/com/craftify/backend/MainApplication.java
@@ -5,9 +5,6 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
 import java.util.TimeZone;
 
 import static java.time.ZoneOffset.UTC;
@@ -16,8 +13,6 @@ import static java.time.ZoneOffset.UTC;
 @OpenAPIDefinition(
         info = @Info(title = "Craftify API", version = "v1"),
         servers = {@Server(url = "http://localhost:8080")})
-@EnableMongoRepositories
-@EnableMongoAuditing
 public class MainApplication {
 
     public static void main(String[] args) {
